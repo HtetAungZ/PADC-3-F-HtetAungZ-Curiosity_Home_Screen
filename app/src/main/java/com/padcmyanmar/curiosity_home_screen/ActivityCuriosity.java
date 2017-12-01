dc1dbea33e07535dfdf77caebb405798ac9c254b
DESKTOP-0DQPE5H\ICE <saberlinger98@gmail.com>
2017-12-01 13:44:49 +0630

    implement curiosity screen

---
+++ app/src/main/java/com/padcmyanmar/curiosity_home_screen/ActivityCuriosity.java
@@ -0,0 +1,13 @@
+package com.padcmyanmar.curiosity_home_screen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityCuriosity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_curiosity);
+    }
+}
